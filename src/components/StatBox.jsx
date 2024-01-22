94971f0de189c65a9ffcef68a905dd383a699221
Michail Petrov <Gismanus@gmail.com>
2024-01-22 11:09:25 +0300

    [StatBox] - fixed

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -7,20 +7,22 @@ const StatBox = ({ title, subtitle, icon, progress, increase }) => {
   const colors = tokens(theme.palette.mode);
 
   return (
-    <Box width="100%" m="0px">
-      <Box display="flex" justifyContent="space-between">
-        <Box>
-          {icon}
-          <Typography
-            variant="h4"
-            fontWeight="bold"
-            sx={{ color: colors.grey[100] }}
-          >
-            {title}
-          </Typography>
-        </Box>
-        <Box>
-            <ProgressCircle progress={progress} />
+    <Box width="100%" m="20px 10px">
+      <Box display="flex" justifyContent="space-between" flexDirection="column" height="100%">
+        <Box display="flex" justifyContent="space-between">
+            <Box>
+              {icon}
+              <Typography
+                variant="h4"
+                fontWeight="bold"
+                sx={{ color: colors.grey[100] }}
+              >
+                {title}
+              </Typography>
+            </Box>
+            <Box>
+                <ProgressCircle progress={progress} />
+            </Box>
         </Box>
         <Box display="flex" justifyContent="space-between">
         <Typography
